f2306a49265e9fa501f7ba51b28786c76ff99711
Kelly Atmore <kelly.atmore47@gmail.com>
2022-05-02 18:02:47 -0700

    add group by

--- 2_queries_joins/2_cohorts_total_assignment_duration.sql
+++ 2_queries_joins/2_cohorts_total_assignment_duration.sql
@@ -3,3 +3,11 @@ FROM assignment_submissions
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name = 'FEB12';
+
+
+
+SELECT students.name as student, count(assignment_submissions.*) as total_submissions
+FROM assignment_submissions
+WHERE students.end_date = NULL
+JOIN students ON students.id = student_id
+GROUP BY students.name;
